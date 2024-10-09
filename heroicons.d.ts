fbf6d14d85290ed59a34f429c17e2ee9603b2f6d
Aditya Mishra <adityamishra5889@gmail.com>
2024-10-09 23:49:49 +0530

    initial commit

---
+++ heroicons.d.ts
@@ -0,0 +1,12 @@
+declare module '@heroicons/react/solid' {
+    import { ComponentType, SVGProps } from 'react';
+    const icons: Record<string, ComponentType<SVGProps<SVGSVGElement>>>;
+    export default icons;
+  }
+  
+  declare module '@heroicons/react/outline' {
+    import { ComponentType, SVGProps } from 'react';
+    const icons: Record<string, ComponentType<SVGProps<SVGSVGElement>>>;
+    export default icons;
+  }
+  
---
+++ public/images/index.tsx
@@ -0,0 +1,10 @@
+import calendar from "./calendar.svg";
+import mark from "./mark.svg";
+import leftRight from "./Frame.svg";
+import dropdown from "./Frame-1.svg";
+export{
+    calendar,
+    mark,
+    leftRight,
+    dropdown
+}
